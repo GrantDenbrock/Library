a22d87c331658402993aeff55fc1fc8314e6d23b
Ian Kenney <ian.kenney@asu.edu>
2016-06-01 11:47:19 -0700

    Added algorithm list to library

--- src/Library.py
+++ src/Library.py
@@ -1,15 +1,26 @@
 import Book
+from Sorting import *
 
 class Library(object):
-	def __init__(self):
-		self.shelf=[]
-	
-	def add_books(self,books):
-		for book in books:
-			self.shelf.append(book)
+    sorting_algos = {'merge': merge,
+                     'count': count,
+                     'select': select,
+                     'bubble':bubble,
+                    }
 
-	def generate_books(self,n):
-		import random
-		self.add_books([random.randint(100000,999999) for _ in range(n)])
-		
-		
+    def __init__(self):
+        self.shelf=[]
+    
+    def add_books(self,books):
+        for book in books:
+            self.shelf.append(book)
+
+    def generate_books(self,n):
+        import random
+        self.add_books([random.randint(100000,999999) for _ in range(n)])
+        
+    def sort(self,algo='merge'):
+        self.shelf = self.sorting_algos[algo](self.shelf)
+
+    def __str__(self):
+        return str(self.shelf)
