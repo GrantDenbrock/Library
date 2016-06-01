c5de73024ed24a87dc704e4fe190cbe249094e3c
Grant Denbrock <gdenbroc@asu.edu>
2016-06-01 02:00:26 -0700

    added generate_books() to Library.py

--- src/Library.py
+++ src/Library.py
@@ -1,9 +1,15 @@
 import Book
 
-class library(self):
+class Library(self):
 	def __init__(self):
 		self.shelf=[]
 	
-	def addbook(self,books):
+	def add_books(self,books):
 		for book in books:
 			self.shelf.append(book)
+
+	def generate_books(self,n):
+		import random
+		self.add_books([random.randint(100000,999999) for _ in range(n)])
+		
+		
